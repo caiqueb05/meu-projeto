99ab9e50b9120f5f75c63e057f2f46349be47d83
Caique Bezerra <caique.bezerra@gmail.com>
2021-05-05 19:09:49 -0300

    meu primeiro commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+solucao(10)
+
+function solucao(numero) {
+	//seu codigo aqui
+    
+    for (var i = numero; i < numero.lenght; i--) {          
+        if (numero[i] == 0) {
+        console.log("KABUM")        
+        } else {
+            console.log(numero[i])
+        }
+    }
+    
+    
+} 
+//
